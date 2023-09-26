43b15d384b7999d607664feadcb01bb4e4d99620
Benjamin <bdevans@sun.ac.za>
2023-09-26 12:01:28 +0200

    Init commit with pp working

---
+++ f1tenth_controllers/f1tenth_sim/dynamics_simulator.py
@@ -0,0 +1,102 @@
+import numpy as np
+from numba import njit
+
+from f1tenth_sim.dynamic_models import vehicle_dynamics_st, pid
+
+
+
+class DynamicsSimulator:
+    def __init__(self, seed, timestep):
+        self.seed = seed
+        self.time_step = timestep
+
+        # These parameters are related to the vehicle model...,not simulation.
+        self.params = {'mu': 1.0489, 'C_Sf': 4.718, 'C_Sr': 5.4562, 'lf': 0.15875, 'lr': 0.17145, 'h': 0.074, 'm': 3.74, 'I': 0.04712, 's_min': -0.4189, 's_max': 0.4189, 'sv_min': -3.2, 'sv_max': 3.2, 'v_switch': 7.319, 'a_max': 9.51, 'v_min':-5.0, 'v_max': 20.0, 'width': 0.31, 'length': 0.58}
+
+        # state is [x, y, steer_angle, vel, yaw_angle, yaw_rate, slip_angle]
+        self.state = np.zeros((7, ))
+
+        # control inputs
+        self.accel = 0.0
+        self.steer_angle_vel = 0.0
+
+        # steering delay buffer
+        self.steer_buffer = np.empty((0, ))
+        self.steer_buffer_size = 2
+
+    def update_pose(self, raw_steer, vel):
+        """
+        Steps the vehicle's physical simulation
+
+        Args:
+            steer (float): desired steering angle
+            vel (float): desired longitudinal velocity
+
+        Returns:
+            current_scan
+        """
+        # state is [x, y, steer_angle, vel, yaw_angle, yaw_rate, slip_angle]
+
+        # steering delay
+        steer = 0.
+        if self.steer_buffer.shape[0] < self.steer_buffer_size:
+            steer = 0.
+            self.steer_buffer = np.append(raw_steer, self.steer_buffer)
+        else:
+            steer = self.steer_buffer[-1]
+            self.steer_buffer = self.steer_buffer[:-1]
+            self.steer_buffer = np.append(raw_steer, self.steer_buffer)
+
+        # steering angle velocity input to steering velocity acceleration input
+        accl, sv = pid(vel, steer, self.state[3], self.state[2], self.params['sv_max'], self.params['a_max'], self.params['v_max'], self.params['v_min'])
+        
+        mu_std = 0.2 #!Big problem
+        #TODO: this must be a parameter.....
+        sample_mu = self.params['mu'] + np.random.randn() * mu_std
+        f = vehicle_dynamics_st(
+            self.state,
+            np.array([sv, accl]),
+            sample_mu,
+            self.params['C_Sf'],
+            self.params['C_Sr'],
+            self.params['lf'],
+            self.params['lr'],
+            self.params['h'],
+            self.params['m'],
+            self.params['I'],
+            self.params['s_min'],
+            self.params['s_max'],
+            self.params['sv_min'],
+            self.params['sv_max'],
+            self.params['v_switch'],
+            self.params['a_max'],
+            self.params['v_min'],
+            self.params['v_max'])
+        self.state = self.state + self.time_step * f
+        
+        # bound yaw angle
+        if self.state[4] > 2*np.pi:
+            self.state[4] = self.state[4] - 2*np.pi
+        elif self.state[4] < 0:
+            self.state[4] = self.state[4] + 2*np.pi
+
+        return self.state
+
+    def reset(self, pose):
+        """
+        Resets the vehicle to a pose
+        
+        Args:
+            pose (np.ndarray (3, )): pose to reset the vehicle to
+
+        Returns:
+            None
+        """
+        # clear control inputs
+        self.accel = 0.0
+        self.steer_angle_vel = 0.0
+        # clear state
+        self.state = np.zeros((7, ))
+        self.state[0:2] = pose[0:2]
+        self.state[4] = pose[2]
+        self.steer_buffer = np.empty((0, ))
